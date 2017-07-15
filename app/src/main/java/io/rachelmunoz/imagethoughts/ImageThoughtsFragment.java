eefdda7d2f2032994f2024b912516f897479617a
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-15 15:31:46 -0700

    Adds listener to EditText for ImageThought, adds height to EditText in fragment layouts, adds default text for imageThought text

--- app/src/main/java/io/rachelmunoz/imagethoughts/ImageThoughtsFragment.java
+++ app/src/main/java/io/rachelmunoz/imagethoughts/ImageThoughtsFragment.java
@@ -3,6 +3,8 @@ package io.rachelmunoz.imagethoughts;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,7 +26,7 @@ public class ImageThoughtsFragment extends Fragment {
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		mImageThought = new ImageThought(getString(R.string.thought_text));
+		mImageThought = new ImageThought(getString(R.string.thought_text_default));
 	}
 
 	@Override
@@ -33,6 +35,22 @@ public class ImageThoughtsFragment extends Fragment {
 
 		mImageThoughtEditText = (EditText) view.findViewById(R.id.imageThought_text);
 		mImageThoughtEditText.setText(mImageThought.getThought());
+		mImageThoughtEditText.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+				mImageThought.setThought(charSequence.toString());
+			}
+
+			@Override
+			public void afterTextChanged(Editable editable) {
+
+			}
+		});
 
 		mImageThoughtDateTextView = (TextView) view.findViewById(R.id.imageThought_date);
 		mImageThoughtDateTextView.setText(mImageThought.getFormattedDate());
