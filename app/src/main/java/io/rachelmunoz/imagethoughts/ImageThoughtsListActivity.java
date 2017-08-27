17c6db568dedb4620458c19102d0db9447603b11
rachelmunoz <rachelumunoz>
2017-08-27 14:43:53 -0700

    Adds Callbacks interface to communicate with host Activity

--- app/src/main/java/io/rachelmunoz/imagethoughts/ImageThoughtsListActivity.java
+++ app/src/main/java/io/rachelmunoz/imagethoughts/ImageThoughtsListActivity.java
@@ -1,5 +1,7 @@
 package io.rachelmunoz.imagethoughts;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
@@ -7,7 +9,7 @@ import android.support.v7.app.AppCompatActivity;
  * Created by rachelmunoz on 7/21/17.
  */
 
-public class ImageThoughtsListActivity extends SingleFragmentActivity {
+public class ImageThoughtsListActivity extends SingleFragmentActivity implements ImageThoughtsListFragment.Callbacks {
 
 	@Override
 	public Fragment createFragment() {
@@ -17,5 +19,19 @@ public class ImageThoughtsListActivity extends SingleFragmentActivity {
 	@Override
 	protected int getLayoutResId() {
 		return R.layout.activity_masterdetail;
+	}
+
+	@Override
+	public void onImageThoughtSelected(ImageThought imageThought) {
+		if (findViewById(R.id.detail_fragment_container) == null){
+			Intent intent = ImageThoughtsActivity.newIntent(this, imageThought.getId());
+			startActivity(intent);
+	 	} else {
+			Fragment newDetail = ImageThoughtsFragment.newInstance(imageThought.getId());
+
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.detail_fragment_container, newDetail)
+					.commit();
+		}
 	}
 }
